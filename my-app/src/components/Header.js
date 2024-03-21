8a24bc663d1b716cb720cf8268a53b08f63f336d
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-21 21:15:41 +0300

    0.0.4

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
+        <div className='alien'></div>
         <div>
             Галактический интернет магазин планет Солнечной системы
         </div>
